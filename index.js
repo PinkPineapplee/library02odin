17abcc11d9901a087d6c7904928f849a059b4315
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-03-14 05:42:56 +0100

    add eventlistener to add  new book button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+  const addNewBookBtn = document.querySelector(button)
+
 const library = new Array();
 
 //create a book constructor
@@ -9,12 +11,16 @@ function Book(id, title,author,pages, read=false){
     this.read = read;
 
 }
+console.log(Book);
 
 //function that add new book to library array
 function addNewBook(title,author,pages){
-    const newBook = new Book()
+    const newBook = new Book();
       
     return library.push(newBook);
 }
 
-//
+// add event listener to get input values
+ addNewBookBtn.addEventListener("click",()=>{
+    addNewBook()
+ })
