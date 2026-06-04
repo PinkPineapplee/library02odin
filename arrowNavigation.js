c1ae3f3944a07aae45f9de88d3e859f17ba078aa
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-06-04 11:42:36 +0100

    comment out old logic

--- arrowNavigation.js
+++ arrowNavigation.js
@@ -8,58 +8,47 @@
      
       console.log("entered arrow nav function");
 
-      function focusArrow (array){
-        if (clickedArrow.id === "history-left" || 
-          clickedArrow.id === "lib-left" || 
-          clickedArrow.id === "new-left" ||
-          clickedArrow.id === "history-right" || 
-          clickedArrow.id === "lib-right" || 
-          clickedArrow.id === "new-right"){
-            
-          }
-        
-
-      }
     
-      leftArrow.forEach(arrow => arrow.addEventListener("click",(e)=>{
-        let clickedArrow = e.target;
+    
+      // leftArrow.forEach(arrow => arrow.addEventListener("click",(e)=>{
+      //   let clickedArrow = e.target;
 
-        if (clickedArrow.id === "history-left" || 
-          clickedArrow.id === "lib-left" || 
-          clickedArrow.id === "new-left"){
+      //   if (clickedArrow.id === "history-left" || 
+      //     clickedArrow.id === "lib-left" || 
+      //     clickedArrow.id === "new-left"){
 
-      //make library array items move to the left.
-      let item = array.shift(0);
-      gallary.removeChild(item);
+      // //make library array items move to the left.
+      // let item = array.shift(0);
+      // gallary.removeChild(item);
 
-       array.push(item);
-       gallary.appendChild(item);
-       console.log("hi I am left arrow");
-      } else {
-        console.log(clickedArrow.id, " arrow left is not working")
-      }
-       })
-      )
+      //  array.push(item);
+      //  gallary.appendChild(item);
+      //  console.log("hi I am left arrow");
+      // } else {
+      //   console.log(clickedArrow.id, " arrow left is not working")
+      // }
+      //  })
+      // )
 
     
-       rightArrow.forEach(arrow => arrow.addEventListener("click",(e)=>{
-         let clickedArrow = e.target;
+      //  rightArrow.forEach(arrow => arrow.addEventListener("click",(e)=>{
+      //    let clickedArrow = e.target;
 
-        if (clickedArrow.id === "history-right" || 
-          clickedArrow.id === "lib-right" || 
-          clickedArrow.id === "new-right"){
+      //   if (clickedArrow.id === "history-right" || 
+      //     clickedArrow.id === "lib-right" || 
+      //     clickedArrow.id === "new-right"){
 
-      //make array items move to the right.
-        let item = array.pop(array.length);
-         gallary.removeChild(item);
+    //   //make array items move to the right.
+    //     let item = array.pop(array.length);
+    //      gallary.removeChild(item);
 
-         array.unshift(item);
-         gallary.appendChild(item);
-       console.log("hi I am right arrow");
-       }else {
-        console.log(clickedArrow.id," arrow right is not working")
-      }
-       })
+    //      array.unshift(item);
+    //      gallary.appendChild(item);
+    //    console.log("hi I am right arrow");
+    //    }else {
+    //     console.log(clickedArrow.id," arrow right is not working")
+    //   }
+    //    })
        
-    );
+    // );
   }
