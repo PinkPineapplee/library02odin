47463d58667d3273d804b7bb3adadd6ffd6347a0
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-06-05 07:41:45 +0100

    refactor arrow Nav func

--- arrowNavigation.js
+++ arrowNavigation.js
@@ -1,81 +1,66 @@
  const leftArrow = document.querySelectorAll(".left");
   const rightArrow = document.querySelectorAll(".right");
   const arrows = document.querySelectorAll(".arrow");
-
+  const libLeft = document. querySelector("#lib-left");
+  const librit = document. querySelector("#lib-right");
 
 //make books move from right to left
    export function arrowNavigation(array, gallary){
      
       console.log("entered arrow nav function");
 
-     let currentIndex = 0;
+  //    let currentIndex = 0;
     
-     function updateDisplay(){
-      gallary.textContent = array[currentIndex];
-     }
-    
-
-   leftArrow.addEventListener("click",()=>{
-      if (currentIndex > 0){
-        currentIndex--;
-      }else{
-        currentIndex = array.length - 1;
-      }
-      updateDisplay();
-   })
-
-
-   rightArrow.addEventListener("clicks",()=>{
-     if (currentIndex < array.lenght - 1){
-      currentIndex++
-     }else {
-      currentIndex = 0;
-     }
-     updateDisplay();
-   })
-
+  //    function updateDisplay(){
+  //     gallary.textContent = array[currentIndex];
+  //    }
     
 
-    
-      // leftArrow.forEach(arrow => arrow.addEventListener("click",(e)=>{
-      //   let clickedArrow = e.target;
+  //  libLeft.addEventListener("click",()=>{
+  //     if (currentIndex > 0){
+  //       currentIndex--;
+  //     }else{
+  //       currentIndex = array.length - 1;
+  //     }
+  //     updateDisplay();
+  //  })
 
-      //   if (clickedArrow.id === "history-left" || 
-      //     clickedArrow.id === "lib-left" || 
-      //     clickedArrow.id === "new-left"){
 
-      // //make library array items move to the left.
-      // let item = array.shift(0);
-      // gallary.removeChild(item);
-
-      //  array.push(item);
-      //  gallary.appendChild(item);
-      //  console.log("hi I am left arrow");
-      // } else {
-      //   console.log(clickedArrow.id, " arrow left is not working")
-      // }
-      //  })
-      // )
+  //  librit.addEventListener("clicks",()=>{
+  //    if (currentIndex < array.lenght - 1){
+  //     currentIndex++;
+  //    }else {
+  //     currentIndex = 0;
+  //    }
+  //    updateDisplay();
+  //  })
 
     
-      //  rightArrow.forEach(arrow => arrow.addEventListener("click",(e)=>{
-      //    let clickedArrow = e.target;
 
-      //   if (clickedArrow.id === "history-right" || 
-      //     clickedArrow.id === "lib-right" || 
-      //     clickedArrow.id === "new-right"){
-
-    //   //make array items move to the right.
-    //     let item = array.pop(array.length);
-    //      gallary.removeChild(item);
-
-    //      array.unshift(item);
-    //      gallary.appendChild(item);
-    //    console.log("hi I am right arrow");
-    //    }else {
-    //     console.log(clickedArrow.id," arrow right is not working")
-    //   }
-    //    })
+    
+      libLeft.addEventListener("click",()=>{
        
-    // );
+      //make library array items move to the left.
+      let item = array.shift(0);
+      gallary.removeChild(item);
+
+       array.push(item);
+       gallary.appendChild(item);
+       console.log("hi I am left arrow");
+      
+       })
+      
+
+    
+       librit.addEventListener("clicks",()=>{
+       
+      //make array items move to the right.
+        let item = array.pop(array.length);
+         gallary.removeChild(item);
+
+         array.unshift(item);
+         gallary.appendChild(item);
+       console.log("hi I am right arrow"); 
+       })
+         
   }
