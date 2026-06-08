8948f9eb20657e6ff61757b4774b39fc55c4cd38
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-06-08 05:47:31 +0100

    update arrow nav

--- arrowNavigation.js
+++ arrowNavigation.js
@@ -9,64 +9,30 @@
      
       console.log("entered arrow nav function");
 
-     let currentIndex = 0;
-    
-     function updateDisplay(action){
-      gallary.textContent += gallary.action(array[currentIndex]);
+      libLeft.addEventListener("click",()=>{
        
-     }
-    
+      //make library array items move to the left.
+      let itemLeft = array.shift(0);
+      console.log(itemLeft)
+      gallary.remove(itemLeft);
 
-   libLeft.addEventListener("click",()=>{
-    for( i = array.length; i <= array.length; i--){
-      if (currentIndex > 0){
-        currentIndex--;
-      }else{
-        currentIndex = array.length - 1;
-      }
-      updateDisplay(appendChild);
-    }
-   })
-
-
-   librit.addEventListener("clicks",()=>{
-    for (i = 0; i >= array.length; i++){
-     if (currentIndex < array.length - 1){
-      currentIndex++;
-     }else {
-      currentIndex = 0;
-     }
-     updateDisplay(remove);
-    }
-   })
-
-    
-
-    
-    //   libLeft.addEventListener("click",()=>{
-       
-    //   //make library array items move to the left.
-    //   let item = array.shift(0);
-    //   console.log(item)
-    //  // gallary.remove(item);
-
-    //    array.push(item);
-    //   // gallary.appendChild(item);
-    //    console.log("hi I am left arrow");
+       array.push(itemLeft);
+       gallary.appendChild(itemLeft);
+       console.log("hi I am left arrow");
       
-    //    })
+       })
       
 
     
-    //    librit.addEventListener("clicks",()=>{
+       librit.addEventListener("clicks",()=>{
        
-    //   //make array items move to the right.
-    //     let item = array.pop(array.length);
-    //      gallary.remove(item);
+      //make array items move to the right.
+        let itemRit = array.pop(array.length - 1);
+         gallary.remove(itemRit);
 
-    //      array.unshift(item);
-    //      gallary.appendChild(item);
-    //    console.log("hi I am right arrow"); 
-    //    })
+         array.unshift(itemRit);
+         gallary.appendChild(itemRit);
+       console.log("hi I am right arrow"); 
+        })
          
   }
